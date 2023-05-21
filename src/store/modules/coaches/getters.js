d479a7f6369137eb0fa11a0db69a59b52800a54c
Peter Labib <peter.adel.labib@gmail.com>
2023-05-21 23:04:39 +0300

    add vuex

---
+++ src/store/modules/coaches/getters.js
@@ -0,0 +1,8 @@
+export default {
+    coachesList(state) {
+        return state.coachesList
+    },
+    hasCoaches(state) {
+        return state
+    }
+}
--- src/views/coaches/CoachList.vue
+++ src/views/coaches/CoachList.vue
@@ -2,8 +2,22 @@
   <section>Filter</section>
   <section>
     <div class="controls">
-        <button>Refresh</button>
-        <RouterLink to="/register">Register as Coach</RouterLink>
+      <button>Refresh</button>
+      <RouterLink to="/register">Register as Coach</RouterLink>
     </div>
+    <ul>
+      <li v-for="coach in filterdCoaches" :key="coach.id"></li>
+    </ul>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filterdCoaches() {
+        console.log(this.$store.getters)
+      return this.$store.getters['coaches/coachesList']
+    }
+  }
+}
+</script>
