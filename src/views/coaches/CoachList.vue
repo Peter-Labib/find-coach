e056d503142586a5c4bb355a1c97793b7a48fd1a
p.labib <p.labib@ditech.ae>
2023-05-22 15:47:02 +0300

    add coachItem component

--- src/views/coaches/CoachList.vue
+++ src/views/coaches/CoachList.vue
@@ -6,19 +6,29 @@
       <RouterLink to="/register">Register as Coach</RouterLink>
     </div>
     <ul v-if="hasCoaches">
-      <li
+      <CoachItem
         v-for="coach in filterdCoaches"
         :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+        :description="coach.description"
+        :rate="coach.hourlyRate"
       >
         {{ coach.firstName }}
-      </li>
+      </CoachItem>
     </ul>
     <h3 v-else>has no coaches</h3>
   </section>
 </template>
 
 <script>
+  import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components:{
+    CoachItem
+  },
   computed: {
     filterdCoaches() {
       return this.$store.getters['coaches/coachesList']
@@ -29,3 +39,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
